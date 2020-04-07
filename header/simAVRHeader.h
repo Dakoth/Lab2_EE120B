275049b83df567c004bbcd92a81c79015fd576a3
Alfredo Gonzalez <agonz250@wch136-11.cs.ucr.edu>
2020-04-07 13:45:57 -0700

    updated header file

--- header/simAVRHeader.h
+++ header/simAVRHeader.h
@@ -14,6 +14,9 @@ const struct avr_mmcu_vcd_trace_t _mytrace[] _MMCU_ = {
     { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } , // Example full port
  
     { AVR_MCU_VCD_SYMBOL("PINA"), .what = (void*)&PINA, }, //added
+    { AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0, .what = (void*)&PINA, }, //added 2
+    
+
 };
 
 /* Function to output through UART */
