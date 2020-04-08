6a55c166cae618b8d5876e55b31e1a301b386619
Alfredo Gonzalez <agonz250@wch136-11.cs.ucr.edu>
2020-04-08 01:11:51 -0700

    Part 2 working

--- source/main.c
+++ source/main.c
@@ -13,47 +13,23 @@
 #include "simAVRHeader.h"
 #endif	
 
-//Used to get value at specified bit value 
-//unsigned char GetBit(unsigned char x, unsigned char k) {
-//	return ((x & (0x01 << k)) != 0);
-//}
-
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
-	//unsigned char tmpC = 0x00; // Temporary variable to hold the value of C
-	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
 
-	unsigned char cntAvail = 0x00;	//Count of number of FREE spaces in binary 
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A 
+	unsigned char cntAvail;	//Count of number of FREE spaces in binary 
 	
 	while(1) {
 		//read input
 		tmpA = PINA;
-		
+
+		tmpA = ~tmpA; 	//invert the bits to show the correct values when added 	
+	
 		//counts number of free spaces in A 
 		cntAvail = ((tmpA & 0x08) >> 3) + ((tmpA & 0x04) >> 2) + ((tmpA & 0x02) >> 1) +	((tmpA & 0x01)); //3, 2, 1, 0
-		
+	
 		PORTC = cntAvail;		
-	}
-	//tmpA = PINA & 0x0F;
-	/*
-	while(1) {
-		// 1) Read input
-		//tmpA = PINA & 0x0F; 	//Selects PA3 - PA0
-		for (i = 0; i < 8; i++) { //if 1, increase count, does it 4 times
-			if( GetBit(tmpA, i) ) {
-				cntavail++;	
-			}
-		}
-		printf("%d\n", cntavail); 	//TEST
-		// 2) Perform computation 
-
-		//tmpA = (tmpA & 0xF0); //clears 
-		
-		// 3 write output
-		//tmpC = cntavail; //added
-		//PORTC = tmpC; //added 
-		PORTC = cntavail;
-	} */	
+	}	
 	return 0;
 }
