78d9a48bcb0db64b7d41b0d85687ce90b1e775a6
Alfredo Gonzalez <agonz250@wch136-11.cs.ucr.edu>
2020-04-08 02:35:40 -0700

    Messing around with main, part 3

--- source/main.c
+++ source/main.c
@@ -19,7 +19,7 @@ int main(void) {
 
 	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A 
 	unsigned char cntAvail;	//Count of number of FREE spaces in binary 
-	unsigned char full = 0x00;	
+	//unsigned char full = 0x00;	
 
 	while(1) {
 		//read input
@@ -28,14 +28,17 @@ int main(void) {
 
 		//If the lot is full //PART 3
 		//if (A3 & ... & A0 = 1), then full should be make b7 = 1 
-		full = ((tmpA & 0x08) << 4) & ((tmpA & 0x04) << 5) & ((tmpA & 0x02) << 6) & ((tmpA & 0x01) << 7); //	 
+		//full = ((tmpA & 0x08) << 4) & ((tmpA & 0x04) << 5) & ((tmpA & 0x02) << 6) & ((tmpA & 0x01) << 7); //	 
 	
 		tmpA = ~tmpA; 	//invert the bits to show the correct values when added 	
 		//counts number of free spaces in A 
-		cntAvail = ((tmpA & 0x08) >> 3) + ((tmpA & 0x04) >> 2) + ((tmpA & 0x02) >> 1) +	((tmpA & 0x01)); //3, 2, 1, 0
-
+		cntAvail = ((tmpA & 0x08) >> 3) + ((tmpA & 0x04) >> 2) + ((tmpA & 0x02) >> 1) +	((tmpA & 0x01)); //3, 2, 1, 0	
+		PORTC = cntAvail; //| full;		
 		
-		PORTC = cntAvail | full;		
+		//If the lot is full, then set B7 = 1
+		if (cntAvail == 0x00) {
+			PORTC = cntAvail | 0x80;
+		}
 	}	
 	return 0;
 }
