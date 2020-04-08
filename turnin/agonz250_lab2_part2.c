6746d61fe4b6308daf812f6bf6bdb0c44076a209
Alfredo Gonzalez <agonz250@wch136-11.cs.ucr.edu>
2020-04-08 01:32:37 -0700

    Completed part 2

---
+++ turnin/agonz250_lab2_part2.c
@@ -0,0 +1,35 @@
+/*	Author: agonz250
+ *  Partner(s) Name: 
+ *	Lab Section: 028 
+ *	Assignment: Lab #2  Exercise # 2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif	
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
+
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A 
+	unsigned char cntAvail;	//Count of number of FREE spaces in binary 
+	
+	while(1) {
+		//read input
+		tmpA = PINA;
+
+		tmpA = ~tmpA; 	//invert the bits to show the correct values when added 	
+	
+		//counts number of free spaces in A 
+		cntAvail = ((tmpA & 0x08) >> 3) + ((tmpA & 0x04) >> 2) + ((tmpA & 0x02) >> 1) +	((tmpA & 0x01)); //3, 2, 1, 0
+	
+		PORTC = cntAvail;		
+	}	
+	return 0;
+}
