d18227b54dcac18752785ee391cac2be4654ad86
Alfredo Gonzalez <agonz250@wch136-11.cs.ucr.edu>
2020-04-07 18:25:27 -0700

    Updated main once again, should work?

--- source/main.c
+++ source/main.c
@@ -13,6 +13,11 @@
 #include "simAVRHeader.h"
 #endif	
 
+//Used to get value at specified bit value 
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
+
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
@@ -20,16 +25,22 @@ int main(void) {
 	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
 
 	unsigned char cntavail = 0;	//Count of number of spaces in binary 
+	//tmpA = PINA & 0x0F;
 
 	while(1) {
 		// 1) Read input
-		tmpA = PINA & 0x0F; 	//Selects PA3 - PA0				
+		//tmpA = PINA & 0x0F; 	//Selects PA3 - PA0
+		for (int i = 0; i < 4; i++) { //if 1, increase count, does it 4 times
+			if(GetBit(A, i)) {
+				cntavail++;	
+			}
+		}
 		// 2) Perform computation 
 
-		tmpA = (tmpA & 0xF0); //clears 
+		//tmpA = (tmpA & 0xF0); //clears 
 		
 		// 3 write output
 		PORTC = cntavail;
 	}
 	return 0;
-}		//test	
+}
