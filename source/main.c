85c346e627f656fde6a1d1f3b02e98e8a6c526a4
Alfredo Gonzalez <agonz250@wch136-11.cs.ucr.edu>
2020-04-07 18:14:40 -0700

    updated main

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: agonz250
  *  Partner(s) Name: 
  *	Lab Section: 028 
- *	Assignment: Lab #2  Exercise #
+ *	Assignment: Lab #2  Exercise # 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -15,28 +15,21 @@
 
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
+	unsigned char tmpC = 0x00; // Temporary variable to hold the value of C
 	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+
+	unsigned char cntavail = 0;	//Count of number of spaces in binary 
 
 	while(1) {
 		// 1) Read input
-		tmpA = PINA & 0x03; 	//Selects PA1 and PA0				
+		tmpA = PINA & 0x0F; 	//Selects PA3 - PA0				
 		// 2) Perform computation 
+
+		tmpA = (tmpA & 0xF0); //clears 
 		
-		if (tmpA == 0x01) { //If PA1 = 0, and PA0 = 1, (ONLY CASE WHERE PB0 = 1)
-			tmpB = (tmpB & 0xFE) | 0x01;	//sets tmp to bbbbbbb0
-							//clears rightmost 1 bit, then set to 1
-		} else {	
-			//if (tmpA == 0x00) { 
-			//// True if PA1 and PA0 are 0
-			//iff PA1 = 1 and PA0 = 0
-			//if PA1 = 1 and PA0 = 1 
-			tmpB = (tmpB & 0xFE) | 0x00; // Sets tmpB to bbbbbbb0
-						     // (clear rightmost bit, then set to 0)	
-		}
 		// 3 write output
-		PORTB = tmpB;
+		PORTC = cntavail;
 	}
 	return 0;
 }		//test	
